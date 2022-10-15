5bc261a0822fbef021342428eb1614f0ee8422d0
Chang-YoungPark <104678555+Chang-YoungPark@users.noreply.github.com>
2022-10-15 09:20:28 +0900

    Add files via upload

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -11,3 +11,18 @@ const now = new Date();
 const getdatetime = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 
 currentdate.innerHTML = getdatetime;
+
+// get the last modified date and time
+const dateTime = document.lastModified;
+// create a template literal to combine the text and date and time
+document.querySelector('#lastMod').textContent  = `Last Updated: ${dateTime}`;
+
+
+//function to create a hamburger menu
+function toggleMenu(){
+    document.getElementById('menuNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
