70d62e35156df0e00570fc8cba1a2fa5f43f2605
Chang-YoungPark <104678555+Chang-YoungPark@users.noreply.github.com>
2022-11-17 22:33:47 +0900

    scripts.js modify

--- lesson10/js/scripts.js
+++ lesson10/js/scripts.js
@@ -4,10 +4,20 @@ const apiURL = ""
 //Go fetch it and then wait for a response.
 fetch(apiURL)
   .then((response) => response.json())
-  .then((changeme) => {
+  .then((weatherInfo) => {
     //Once it comes back, display it to the console.
-    console.log(changeme);
-    
+    console.log(weatherInfo);
+     
+    document.getElementById('place').innerHTML=weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML=weatherInfo.main.temp;
+    document.getElementById('windSpeed').innerHTML=weatherInfo.wind.speed;
+
+    const iconcode = weatherInfo.weather[0].icon;
+    console.log(iconcode);
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(icon_path);
+
+    document.getElementById('weather_icon').src = icon_path;
     
 
  }); //end of "then" fat arrow function
